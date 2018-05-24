374fd8d5ccd1ca6c64bb6f549ab2ad76ddae0991
Moon <3026775966@qq.com>
2018-05-24 21:04:58 +0800

    no message

--- TraderGateWay/src/main/resources/static/js/signin.js
+++ TraderGateWay/src/main/resources/static/js/signin.js
@@ -1,38 +1,50 @@
-jQuery(document).ready(function(){
-    $("#signin").click(function(e){
-        var username;
-        var password;
+/*这个函数在点击登录按钮或者按下回车键后调用*/
+var signin=function(e){
+    var username;
+    var password;
 
-        username=$("input[name='username']").val();
-        password=$("input[name='password']").val();
-        if(username==''){
-            alert("Please input your username");
-            return false;
-        }
-        if(password==''){
-            alert("Please input your password");
-            return false;
-        }
+    username=$("input[name='username']").val();
+    password=$("input[name='password']").val();
+    if(username==''){
+        alert("Please input your username");
+        return false;
+    }
+    if(password==''){
+        alert("Please input your password");
+        return false;
+    }
 
-        $.ajax({
-            type:"post",
-            url:"/user/login",
-            contentType: "application/json",
-            data:JSON.stringify({
-                "username":username,
-                "password":password
-            }),
-            /*期望后端返回数据类型是json*/
-            dataType:"json",
-            /*后端的响应状态码为200时，表示响应成功，触发success*/
-            success:function(data,textStatus){
-                alert("Sign in successfully!");
-            },
-            /*其他响应状态码，触发error，ajax还会在下列情况走error：1.返回数据类型不是json。2.网络中断。3.后台响应中断。*/
-            error:function(){
-                alert("Failed to sign in.You may have entered an incorrect username or password.");
-            }
-        })
+    $.ajax({
+        type:"post",
+        url:"/user/login",
+        contentType: "application/json",
+        data:JSON.stringify({
+            "username":username,
+            "password":password
+        }),
+        /*期望后端返回数据类型是json*/
+        dataType:"json",
+        /*后端的响应状态码为200时，表示响应成功，触发success*/
+        success:function(data,textStatus){
+            alert("Sign in successfully!");
+        },
+        /*其他响应状态码，触发error，ajax还会在下列情况走error：1.返回数据类型不是json。2.网络中断。3.后台响应中断。*/
+        error:function(){
+            alert("Failed to sign in.You may have entered an incorrect username or password.");
+        }
     })
+}
 
-});
+jQuery(document).ready(
+    /*点击登录按钮*/
+    $("#signin").click(signin),
+
+    /*敲击回车键*/
+    $(".form-signin").keypress(function (e){
+        var code = event.keyCode;
+        if (13 == code) {
+            signin()
+        }
+    })
+);
+
--- TraderGateWay/src/main/resources/static/js/signup.js
+++ TraderGateWay/src/main/resources/static/js/signup.js
@@ -1,46 +1,54 @@
-jQuery(document).ready(function(){
-    $("#signup").click(function(e){
-        var username;
-        var password;
-        var confirmpassword;
+var signup=function(e){
+    var username;
+    var password;
+    var confirmpassword;
 
-        username=$("input[name='username']").val();
-        password=$("input[name='password']").val();
-        confirmpassword=$("input[name='confirmpassword']").val();
-        if(username==''){
-            alert("Please create your username");
-            return false;
-        }
-        if(password==''){
-            alert("Please input your password");
-            return false;
-        }
-        if(confirmpassword==''){
-            alert("Please confirm your password");
-            return false;
-        }
-        if(password!=confirmpassword){
-            alert("The two passwords you typed do not match");
-            return false;
-        }
+    username=$("input[name='username']").val();
+    password=$("input[name='password']").val();
+    confirmpassword=$("input[name='confirmpassword']").val();
+    if(username==''){
+        alert("Please create your username");
+        return false;
+    }
+    if(password==''){
+        alert("Please input your password");
+        return false;
+    }
+    if(confirmpassword==''){
+        alert("Please confirm your password");
+        return false;
+    }
+    if(password!=confirmpassword){
+        alert("The two passwords you typed do not match");
+        return false;
+    }
 
-        $.ajax({
-            type:"post",
-            url:"/user/create",
-            contentType: "application/json",
-            data:JSON.stringify({
-                "username":username,
-                "password":password
-            }),
-            dataType:"json",
-            success:function(resp){
-                alert("Sign up successfully!");
-                window.location.href = "signin.html";
-            },
-            error:function(){
-                alert("Failed to sign up");
-            }
-        })
+    $.ajax({
+        type:"post",
+        url:"/user/create",
+        contentType: "application/json",
+        data:JSON.stringify({
+            "username":username,
+            "password":password
+        }),
+        dataType:"json",
+        success:function(resp){
+            alert("Sign up successfully!");
+            window.location.href = "signin.html";
+        },
+        error:function(){
+            alert("Failed to sign up");
+        }
+    })
+}
+
+jQuery(document).ready(
+    $("#signup").click(signup),
+    $(".form-signup").keypress(function (e){
+        var code = event.keyCode;
+        if (13 == code) {
+            signup()
+        }
     })
 
-});
+);
